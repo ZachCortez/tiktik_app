a56bf35a79b42ad1a74f472a563d8c04b26c57d2
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2022-07-27 15:51:13 -0700

    Add files via upload

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -6,8 +6,15 @@ import { topics } from '../utils/constants';
 
 
 const Discover = () => {
+    const router = useRouter();
+    const { topic } = router.query;
+
+    const activeTopicStyle='xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#FF1997]'
+
+    const topicStyle='xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black'
+
   return (
-    <div className='cl:border-b-2 xl:border-gray-200 pb-6'>
+    <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
         <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
             Popular Topics
         </p>
@@ -15,11 +22,11 @@ const Discover = () => {
             {topics.map((item) => (
                 <Link href={`/?topics=${item.name}`} key=
                 {item.name}>
-                    <div>
-                        <span>
+                    <div className={ topic === item.name ? activeTopicStyle : topicStyle}>
+                        <span className='font-bold text-2xl xl:text-md'>
                             {item.icon}
                         </span>
-                        <span>
+                        <span className='font-meduim text-md hidden xl:block capitalize'>
                             {item.name}
                         </span>
                     </div>
