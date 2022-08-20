3f266a3f5669cab7e1194a20fcb75f5f9f2c8241
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2022-08-19 23:00:04 -0700

    Add files via upload

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -8,6 +8,7 @@ import { BsFillPlayFill } from 'react-icons/bs';
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import axios from 'axios';
 import { Video } from '../../types';
+import useAuthStore from '../../store/authStore';
 
 import { BASE_URL } from '../../utils';
 
@@ -21,6 +22,7 @@ const Detail = ({ postDetails }: IProps) => {
     const [ isVideoMuted, setIsVideoMuted ] = useState(false);
     const videoRef = useRef<HTMLVideoElement>(null);
     const router = useRouter();
+    const { userProfile } = useAuthStore();
 
 
     const onVideoClick = () => {
@@ -42,7 +44,7 @@ const Detail = ({ postDetails }: IProps) => {
         if(!post) return null;
 
     return (
-        <div className='flex w-full aboslute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+        <div className='flex w-full aboslute left-0 top-0 bg-gray-700 flex-wrap lg:flex-nowrap'>
             <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
                 <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
                     <p className='cursour-pointer'
@@ -52,7 +54,7 @@ const Detail = ({ postDetails }: IProps) => {
                     </p>
                 </div>
                 <div className='relative'>
-                    <div className='lg:h-[100vh] h-[60vh]'>
+                    <div className='lg:h-[100%] h-[60vh]'>
                         <video
                             ref={videoRef}
                             loop
@@ -91,9 +93,46 @@ const Detail = ({ postDetails }: IProps) => {
 
             <div className='relative w-[1000px] md:w-900px] lg:w-[700px]'>
                     <div className='lg:mt-20 mt-10'>
+                        <div className=" flex gap-3 p-2 cursor-pointer font-semibold rounded">
+                            <div className="ml-4 md:w-20 md:h-20 w-16 h-16">
+                                <Link href={`/profile/${post.postedBy._id}`}>
+                                    <>
+                                    <Image 
+                                        width={62}
+                                        height={62}
+                                        className="rounded-full"
+                                        src={post.postedBy.image}
+                                        alt="profile phoot"
+                                        layout="responsive"
+                                        />
+                                    </>
+                                </Link>
+                            </div>
+                        <div>
+                            <Link href={`/profile/${post.postedBy._id}`}>
+                                <div className="mt-3 flex flex-col gap-2">
+                                    <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                                        {post.postedBy.userName} {` `}
+                                        <GoVerified className="text-blue-400 text-md"/>
+                                    </p>
+                                    <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                                    {post.postedBy.userName}
+                                    </p>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
 
+                    <p className='px-10 text-lg text-gray-600'>
+                        {post.caption}
+                    </p>
 
-
+                    <div className='mt-10 px-10'>
+                        {/* {userProfile && (
+                            <LikeButton />
+                        )} */}
+                    </div>
+                    {/* <Comments /> */}
 
 
 
