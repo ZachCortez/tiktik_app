ef7c4f0522d0bed6c00923fe1cc5436275bf8c89
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2022-09-26 10:26:36 -0700

    Add files via upload

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { uuid } from 'uuidv4';
 
-
-
 import { client } from '../../utils/client';
 
     export default async function handler( req: NextApiRequest,
